af388df33f8a99e774b84bfac9bbc2a4045800d0
chibaibuki <ibuchan1122@icloud.com>
2023-06-08 12:19:41 +0900

    ログイン画面　画面完成

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect,url_for,request
 import random
 
 app = Flask(__name__)
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def sample_top():
     return render_template('index.html')
+
+@app.route('/login',methods=['POST'])
+def login():
+    email=request.form.get('email')
+    pw=request.form.get('pw')
+    return render_template('top.html',email=email,pw=pw)
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
